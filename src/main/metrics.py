aed6a2e29bc3a84544f1d1c43f39f62bd58f7a1d
franckjay <franckjay@gmail.com>
2021-01-01 17:24:51 -0800

    Add Metrics and more unit tests

---
+++ src/main/metrics.py
@@ -0,0 +1,13 @@
+def hit_rate_k(pred_df, actual_col="item_id", pred_col="predictions"):
+    """
+    :param pred_df: DataFrame containing what a user actually interacted with and a predicted list
+    :param actual_col: Column that has what the user actually engaged in
+    :param pred_col: Column name that has the predictions in list form
+    :return: Fractional hit rate for any predictions in `pred_col`
+    """
+
+    pred_df["hit"] = [
+        pred[0] in actual for pred, actual in pred_df[[pred_col, actual_col]].values
+    ]
+
+    return pred_df["hit"].mean()
---
+++ src/test/test_hitrate.py
@@ -0,0 +1,101 @@
+import unittest
+import pandas as pd
+from main.metrics import hit_rate_k
+
+
+class TestBasicMetrics(unittest.TestCase):
+    def setUp(self):
+        self.all_misses = pd.DataFrame(
+            {
+                "predicted": [
+                    ["jay"],
+                    ["jay"],
+                    ["jay"],
+                    ["esther"],
+                    ["esther"],
+                    ["harald"],
+                    ["harald"],
+                    ["harald"],
+                ],
+                "item": [
+                    "The Matrix",
+                    "Arrival",
+                    "Wall-E",
+                    "Mulan",
+                    "Wall-E",
+                    "Mulan",
+                    "Black Beauty",
+                    "Incredibles",
+                ],
+            }
+        )
+
+        self.all_hits = pd.DataFrame(
+            {
+                "predicted": [
+                    ["The Matrix"],
+                    ["Arrival"],
+                    ["Wall-E"],
+                    ["Mulan"],
+                    ["Wall-E"],
+                    ["Mulan"],
+                    ["Black Beauty"],
+                    ["Incredibles"],
+                ],
+                "item": [
+                    "The Matrix",
+                    "Arrival",
+                    "Wall-E",
+                    "Mulan",
+                    "Wall-E",
+                    "Mulan",
+                    "Black Beauty",
+                    "Incredibles",
+                ],
+            }
+        )
+
+        self.partial_hits = pd.DataFrame(
+            {
+                "predicted": [
+                    ["The Shining", "Blade Runner"],
+                    ["Oh Brother, Where art Thou?", "Arrival"],
+                    ["The Lion King", "Harry Potter"],
+                    ["Mulan", "Cats"],
+                    ["Wall-E", "Wall-E"],
+                    ["Mulan", "Wall-E"],
+                    ["Tron", "Tron Legacy"],
+                    ["Incredibles", "Incredibles 2"],
+                ],
+                "item": [
+                    "The Matrix",
+                    "Arrival",
+                    "Wall-E",
+                    "Mulan",
+                    "Wall-E",
+                    "Mulan",
+                    "Black Beauty",
+                    "Incredibles",
+                ],
+            }
+        )
+
+    def test_all_hits(self):
+        self.assertTrue(
+            hit_rate_k(self.all_hits, actual_col="item", pred_col="predicted") == 1.0
+        )
+
+    def test_all_misses(self):
+        self.assertTrue(
+            hit_rate_k(self.all_misses, actual_col="item", pred_col="predicted") == 0.0
+        )
+
+    def test_partial_misses(self):
+        self.assertTrue(
+            hit_rate_k(self.partial_hits, actual_col="item", pred_col="predicted")
+            == 0.5
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
